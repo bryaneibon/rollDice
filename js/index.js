0f872842aff03c66a00befc76db054010756cbb6
bryaneibon <bryaneibon@hotmail.fr>
2024-07-15 16:29:41 -0400

    JS logic review

--- js/index.js
+++ js/index.js
@@ -1,42 +1,13 @@
 var player1Name = prompt("1st player name: ");
 var player2Name = prompt("2nd player name: ");
 
-var player1Score = 0;
-var player2Score = 0;
-
-function randomDiceNumber() {
-    return Math.ceil(Math.random() * 6);
-}
-
-function whoWins(player1Dice, player2Dice) {
-    if (player1Dice > player2Dice) {
-        document.querySelector(".reflesh").innerHTML = "🚩 " + player1Name + " Wins";
-        return "P1W";
-    } else if (player2Dice > player1Dice) {
-        document.querySelector(".reflesh").innerHTML = "🚩 " + player2Name + " Wins";
-        return "P2W";
-    } else {
-        document.querySelector(".reflesh").innerHTML = "🚩 DRAW 🚩";
-        return "DRAW";
-    }
-}
-
-function compileScore(player1Dice, player2Dice) {
-    var theWinner = whoWins(player1Dice, player2Dice);
-
-    if (theWinner === "P1W") {
-        player1Score += 1;
-    } else if (theWinner === "P2W") {
-        player2Score += 1;
-    }
-    document.querySelector(".player1").innerHTML = player1Name + "'s score : " + player1Score;
-    document.querySelector(".player2").innerHTML = player2Name + "'s score : " + player2Score;
-}
-
 document.querySelector(".player1").innerHTML = player1Name;
 document.querySelector(".player2").innerHTML = player2Name;
 
-document.getElementById("rollDiceButton").addEventListener("click", function() {
+var player1Score = 0;
+var player2Score = 0;
+
+document.getElementById("rollDice").addEventListener("click", function() {
     var dice1 = randomDiceNumber();
     var dice2 = randomDiceNumber();
 
@@ -54,3 +25,30 @@ document.getElementById("resetScore").addEventListener("click", function() {
     document.querySelector(".player1").innerHTML = player1Name + "'s score : " + player1Score;
     document.querySelector(".player2").innerHTML = player2Name + "'s score : " + player2Score;
 });
+
+function randomDiceNumber() {
+    return Math.ceil(Math.random() * 6);
+}
+
+function compileScore(player1Dice, player2Dice) {
+    if ("P1W".includes(whoWins(player1Dice, player2Dice))) {
+        player1Score += 1;
+        document.querySelector(".player1").innerHTML = player1Name + "'s score : " + player1Score;
+    } else if ("P2W".includes(whoWins(player1Dice, player2Dice))) {
+        player2Score += 1;
+        document.querySelector(".player2").innerHTML = player2Name + "'s score : " + player2Score;
+    }
+}
+
+function whoWins(player1Dice, player2Dice) {
+    if (player1Dice > player2Dice) {
+        document.querySelector(".reflesh").innerHTML = "🚩 " + player1Name + " Wins";
+        return "P1W";
+    } else if (player2Dice > player1Dice) {
+        document.querySelector(".reflesh").innerHTML = "🚩 " + player2Name + " Wins";
+        return "P2W";
+    } else {
+        document.querySelector(".reflesh").innerHTML = "🚩 DRAW 🚩";
+        return "DRAW";
+    }
+}
